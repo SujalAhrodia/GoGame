699570879c717315279582831f42f38adc55872b
Sujal <sujalahrodia@gmail.com>
2019-07-05 01:25:22 -0400

    Create unique_morse.cpp

---
+++ LeetCode/unique_morse.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> table{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    
+    int uniqueMorseRepresentations(vector<string>& words) 
+    {
+        vector<string> temp;
+        vector<string>::iterator itr;
+        
+        for(string str : words)
+        {
+            string temp1 = "";
+            for(char ch : str)
+                temp1+= table[ch-'a'];
+            temp.push_back(temp1);
+        }
+        //removing duplicates 
+        sort(temp.begin(), temp.end());
+        itr = unique(temp.begin(), temp.end());
+        temp.erase(itr, temp.end());
+
+        return temp.size();       
+    }
+};
