13d807df773a7d07582b93671cc7f2efc2776968
Sujal <sujalahrodia@gmail.com>
2019-07-06 16:29:02 -0400

    Create transpose_matrix.cpp

---
+++ LeetCode/transpose_matrix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) 
+    {
+        int i=A.size();
+        int j=A[0].size();
+        
+        vector<vector<int>> ans(j);
+
+        for(int x=0; x<j; x++)
+        {
+            ans[x] = vector<int> (i);
+            for(int y=0; y<i; y++)
+                ans[x][y] = A[y][x];
+        }
+        return ans;
+    }
+};
