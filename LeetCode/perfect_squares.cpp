688607619b4ab29cf689fbef243a1758422df16e
Sujal <sujalahrodia@gmail.com>
2019-07-09 20:50:39 -0400

    Create perfect_squares.cpp

---
+++ LeetCode/perfect_squares.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSquares(int n) 
+    {
+        int dp[n+1]={0};
+        
+        for(int i=0; i<=n; i++)
+        {
+            dp[i]=i;
+            for(int j=2; j<=sqrt(i); j++)
+                dp[i] = min(dp[i], 1 + dp[i-j*j]);
+        }
+        return dp[n];
+    }
+};
